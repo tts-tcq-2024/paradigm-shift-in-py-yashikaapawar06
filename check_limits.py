71e964c137feae1a83127e42d9b01459cc042ffe
yashikaapawar06 <94790095+yashikaapawar06@users.noreply.github.com>
2024-08-24 11:24:34 +0530

    Covering all test cases

--- check_limits.py
+++ check_limits.py
@@ -20,5 +20,7 @@ def battery_is_ok(temperature, soc, charge_rate):
     return is_temperature_ok(temperature) and is_soc_ok(soc) and is_charge_rate_ok(charge_rate)
 
 if __name__ == '__main__':
-    assert(battery_is_ok(25, 70, 0.7) is True)
-    assert(battery_is_ok(50, 85, 0) is False)
+    assert(battery_is_ok(25, 70, 0.7) is True) #All parameters within range
+    assert(battery_is_ok(50, 85, 0) is False) #Temperature and SOC out of range
+    assert(battery_is_ok(-2,40,0.9) is False) #All parameters are out of range
+    assert(battery_is_ok(0,20,0.8) is True) #Edge values but within range
